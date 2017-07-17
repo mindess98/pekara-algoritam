2de79b3b2ec4f7534fb1e4a9dda224e36f54504c
Imran Polic <imran.polic@gmail.com>
2017-07-17 10:57:37 +0200

    init

---
+++ Algorithms/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Algorithms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] s = textBox1.Text.Split(',');
+            List<int> array = new List<int>();
+            foreach(string t in s)
+            {
+                array.Add(int.Parse(t));
+            }
+
+            myAlgorithms.BubbleSort(array);
+
+           for(int i = 0; i < array.Count; i++)
+            {
+                s[i] = array[i].ToString();
+            }
+
+
+           textBox1.Text = null;
+
+           for (int i = 0; i < s.Length; i++)
+            {
+                textBox1.Text += (s[i] + " "); 
+            }
+             
+        }
+    }
+}
---
+++ Algorithms/myAlgorithms.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public class myAlgorithms
+    {
+        public static void BubbleSort(List<int> array)
+        {
+            for(int i = 0; i < array.Count; i++)
+            {
+                for (int j = 0; j < array.Count - 1; j++)
+                {
+                    if (array[j] < array[j + 1])
+                    {
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
